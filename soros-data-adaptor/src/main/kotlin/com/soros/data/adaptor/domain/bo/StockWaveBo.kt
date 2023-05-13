240d191fe563d8d5109908707eddc905bb0983ef
wangpeng2 <wangpeng2@ushareit.com>
2023-05-13 23:23:07 +0800

    # new table stock wave.

--- soros-data-adaptor/src/main/kotlin/com/soros/data/adaptor/domain/bo/StockWaveBo.kt
+++ soros-data-adaptor/src/main/kotlin/com/soros/data/adaptor/domain/bo/StockWaveBo.kt
@@ -1,8 +1,19 @@
 package com.soros.data.adaptor.domain.bo
 
+import com.soros.data.adaptor.entity.StockHistoryEntity
+import com.soros.data.adaptor.enums.TrendInflectionPointType
+import com.soros.data.adaptor.enums.WaveTypeEnum
 import java.math.BigDecimal
 
 data class StockWaveBo(
         val code: String,
         var range: BigDecimal
+)
+
+data class StockWaveSingleBo(
+    val waveTypeEnum: WaveTypeEnum,
+    val inflectionPointType: TrendInflectionPointType,
+    val inflectionPoint: StockHistoryEntity,
+    val maxPoint: StockHistoryEntity,
+    val minPoint: StockHistoryEntity
 )
---
+++ soros-data-adaptor/src/main/kotlin/com/soros/data/adaptor/utils/SorosUtils.kt
@@ -0,0 +1,11 @@
+package com.soros.data.adaptor.utils
+
+import com.soros.data.adaptor.config.WaveJobConfiguration
+import com.soros.data.adaptor.domain.bo.StockWaveSingleBo
+import com.soros.data.adaptor.entity.StockHistoryEntity
+
+//fun List<StockHistoryEntity>.getStockWaveSingleBo(minInflectionPointDays: Int): StockWaveSingleBo? {
+//    if (this.isEmpty()) {
+//        return null
+//    }
+//}
