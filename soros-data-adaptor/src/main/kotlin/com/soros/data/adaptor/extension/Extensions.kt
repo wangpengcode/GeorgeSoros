cdf5f7df2b016767ff89b38e9eae4965cef1936f
wangpeng2 <wangpeng2@ushareit.com>
2023-05-22 14:19:33 +0800

    # add extension function

--- soros-data-adaptor/src/main/kotlin/com/soros/data/adaptor/extension/Extensions.kt
+++ soros-data-adaptor/src/main/kotlin/com/soros/data/adaptor/extension/Extensions.kt
@@ -2,6 +2,8 @@ package com.soros.data.adaptor.extension
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.google.gson.Gson
+import com.soros.data.adaptor.domain.bo.InflectionPoint
+import com.soros.data.adaptor.enums.InflectionPointType
 import org.springframework.util.CollectionUtils
 import org.springframework.util.StringUtils
 import kotlin.streams.toList
@@ -31,4 +33,20 @@ inline fun <reified T> List<String>.fromJson(): List<T> {
         return emptyList()
     }
     return this.stream().filter { !StringUtils.isEmpty(it) }.map { it.fromJson<T>() }.toList()
+}
+
+fun InflectionPoint.isMax(): Boolean {
+    return this.type == InflectionPointType.MAX
+}
+
+fun InflectionPoint.isMin(): Boolean {
+    return this.type == InflectionPointType.MIN
+}
+
+fun isAllMax(first: InflectionPoint, second: InflectionPoint, third: InflectionPoint): Boolean {
+    return first.type == InflectionPointType.MAX && second.type == InflectionPointType.MAX && third.type == InflectionPointType.MAX
+}
+
+fun isAllMin(first: InflectionPoint, second: InflectionPoint, third: InflectionPoint): Boolean {
+    return first.type == InflectionPointType.MIN && second.type == InflectionPointType.MIN && third.type == InflectionPointType.MIN
 }
