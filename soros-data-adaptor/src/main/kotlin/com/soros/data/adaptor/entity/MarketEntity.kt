8f5022ee800bea1cdb45d0c7d38a945f86369b5d
wangpeng2 <wangpeng2@ushareit.com>
2023-05-23 20:23:06 +0800

    # add market entity

---
+++ soros-data-adaptor/src/main/kotlin/com/soros/data/adaptor/entity/MarketEntity.kt
@@ -0,0 +1,18 @@
+//package com.soros.data.adaptor.entity
+//
+//import lombok.AllArgsConstructor
+//import lombok.NoArgsConstructor
+//import java.math.BigDecimal
+//import javax.persistence.Column
+//import javax.persistence.Entity
+//
+//@Entity(name = "market_history")
+//@AllArgsConstructor
+//@NoArgsConstructor
+//data class MarketEntity(
+//        @Column(name = "date", length = 20)
+//        val date: String,
+//
+//        @Column(name = "st_open")
+//        var open: BigDecimal? = null,
+//)
